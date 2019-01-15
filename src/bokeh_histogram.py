92bd0518dd40246ca65a4ad7bd857a424d376fff
Jeremy <jeremymiller00@gmail.com>
2019-01-15 06:03:59 -0800

    added demonstration code at bottom#

--- src/bokeh_histogram.py
+++ src/bokeh_histogram.py
@@ -5,19 +5,20 @@ from bokeh.io import output_notebook, show, output_file
 from bokeh.models import ColumnDataSource, HoverTool, Panel
 from bokeh.models.widgets import Tabs
 
-# add bsic plot attributes to __init__
-# make git repo
+# add functionality for saving the html
 
 class BokehHistogram():
     '''
-    A class to make interactive histograms with the Bokeh library.
+    A class to simplify the making of interactive histograms with the Bokeh library.
     Requires: Bokeh, Pandas, and Numpy.
-
-    Since no variable definitions will need to be carried across methods, a custom __init__ method is not required.
     '''
-        
-    def hist_hover(self, dataframe, column, bins=30, log_scale=False, 
-                   colors=["SteelBlue", "Tan"], show_plot=True):
+
+    def __init__(self, colors=["SteelBlue", "Tan"], height=600, width=600):
+        self.colors = colors
+        self.height = height
+        self.width = width
+
+    def hist_hover(self, dataframe, column, bins=30, log_scale=False, show_plot=True):
         """
         A method for creating a sinlge Bokeh histogram with hovertool interactivity.
 
@@ -46,14 +47,14 @@ class BokehHistogram():
         if log_scale == True:
             hist_df["log"] = np.log(hist_df[column])
             src = ColumnDataSource(hist_df)
-            plot = figure(plot_height = 600, plot_width = 600,
+            plot = figure(plot_height = self.height, plot_width = self.width,
                   title = "Histogram of {}".format(column.capitalize()),
                   x_axis_label = column.capitalize(),
                   y_axis_label = "Log Count")    
             plot.quad(bottom = 0, top = "log",left = "left", 
-                right = "right", source = src, fill_color = colors[0], 
+                right = "right", source = src, fill_color = self.colors[0], 
                 line_color = "black", fill_alpha = 0.7,
-                hover_fill_alpha = 1.0, hover_fill_color = colors[1])
+                hover_fill_alpha = 1.0, hover_fill_color = self.colors[1])
         else:
             src = ColumnDataSource(hist_df)
             plot = figure(plot_height = 600, plot_width = 600,
@@ -61,9 +62,9 @@ class BokehHistogram():
                   x_axis_label = column.capitalize(),
                   y_axis_label = "Count")    
             plot.quad(bottom = 0, top = column,left = "left", 
-                right = "right", source = src, fill_color = colors[0], 
+                right = "right", source = src, fill_color = self.colors[0], 
                 line_color = "black", fill_alpha = 0.7,
-                hover_fill_alpha = 1.0, hover_fill_color = colors[1])
+                hover_fill_alpha = 1.0, hover_fill_color = self.colors[1])
 
         # hover tool
         hover = HoverTool(tooltips = [('Interval', '@interval'),
@@ -76,7 +77,7 @@ class BokehHistogram():
         else:
             return plot
 
-    def histotabs(self, dataframe, features, colors=['SteelBlue', 'Tan'], log_scale=False, show_plot=False):
+    def histotabs(self, dataframe, features, log_scale=False, show_plot=False):
         '''
         Builds tabbed interface for a series of histograms; calls hist_hover. Specifying 'show_plot=True' will simply display the histograms in sequence rather than in a tabbed interface.
 
@@ -95,13 +96,13 @@ class BokehHistogram():
         '''
         hists = []
         for f in features:
-            h = self.hist_hover(dataframe, f, colors=colors, log_scale=log_scale, show_plot=show_plot)
+            h = self.hist_hover(dataframe, f, log_scale=log_scale, show_plot=show_plot)
             p = Panel(child=h, title=f.capitalize())
             hists.append(p)
         t = Tabs(tabs=hists)
         show(t)
 
-    def filtered_histotabs(self, dataframe, feature, filter_feature, colors=['SteelBlue', 'Tan'], log_scale=False, show_plot=False):
+    def filtered_histotabs(self, dataframe, feature, filter_feature, log_scale=False, show_plot=False):
         '''
         Builds tabbed histogram interface for one feature filtered by another. Feature is numeric, fiter feature is categorical.
 
@@ -121,9 +122,28 @@ class BokehHistogram():
         hists = []
         for col in dataframe[filter_feature].unique():
             sub_df = dataframe[dataframe[filter_feature] == col]
-            histo = self.hist_hover(sub_df, feature, colors=colors, log_scale=log_scale, show_plot=show_plot)
+            histo = self.hist_hover(sub_df, feature, log_scale=log_scale, show_plot=show_plot)
             p = Panel(child = histo, title=col)
             hists.append(p)
         t = Tabs(tabs=hists)
         show(t)
 
+
+######################
+'''
+demontration code
+'''
+
+df = pd.read_csv('/Users/jeremymiller/GoogleDrive/Data_Science/Projects/Education_Data/harvard_ed_x/data/HMXPC13_DI_v2_5-14-14.csv')
+
+df.sample(10)
+
+h = BokehHistogram()
+
+h.hist_hover(df.fillna(0, axis=1), 'nevents', log_scale=False)
+
+h.histotabs(df.fillna(0, axis=1), ['nevents', 'ndays_act', 'nchapters'], log_scale=True)
+
+h.filtered_histotabs(df.fillna(0, axis=1), 'nevents', 'final_cc_cname_DI', log_scale=True)
+
+df.info()

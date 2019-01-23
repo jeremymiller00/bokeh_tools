8016e33c7d5970d0e42678155f654445ee5a25f1
Jeremy <jeremymiller00@gmail.com>
2019-01-23 14:10:45 -0800

    start app.py

---
+++ src/bokeh_app.py
@@ -0,0 +1,14 @@
+from bokeh.io import curdoc
+
+# bokeh app structure
+# create plots and widgets
+
+# add callbacks
+
+# arrange plots and widgets into laiyouts
+
+# call curdoc().add_root(layout)
+
+# execute with
+# bokeh serve --show myapp.py
+

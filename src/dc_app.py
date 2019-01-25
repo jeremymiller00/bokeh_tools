55d4b2f0d6e1fddd79ae25dd3deeb32093e93a74
Jeremy <jeremymiller00@gmail.com>
2019-01-24 22:40:43 -0800

    solve xlim problem

--- src/dc_app.py
+++ src/dc_app.py
@@ -6,37 +6,37 @@ from bokeh.models import ColumnDataSource, HoverTool
 
 df = pd.read_csv('data/gapminder_tidy.csv')
 
-'''
+
 # make basic plot
 # Make the ColumnDataSource: source
-src = ColumnDataSource(data={
-    'x'       : df[df['Year'] == 1970]['fertility'],
-    'y'       : df[df['Year'] == 1970]['life'],
-    'country' : df[df['Year'] == 1970]['Country']
-})
+# src = ColumnDataSource(data={
+#     'x'       : df[df['Year'] == 1970]['fertility'],
+#     'y'       : df[df['Year'] == 1970]['life'],
+#     'country' : df[df['Year'] == 1970]['Country']
+# })
 
 # Create the figure: p
-p = figure(title='1970', x_axis_label='Fertility (children per woman)', y_axis_label='Life Expectancy (years)',
-           plot_height=400, plot_width=700,
-           tools=[HoverTool(tooltips='@country')])
+# p = figure(title='1970', x_axis_label='Fertility (children per woman)', y_axis_label='Life Expectancy (years)',
+#            plot_height=400, plot_width=700,
+#            tools=[HoverTool(tooltips='@country')])
 
 # Add a circle glyph to the figure p
-p.circle(x='x', y='y', source=src)
+# p.circle(x='x', y='y', source=src)
 
 # Output the file and show the figure
-output_file('gapminder.html')
-show(p)
-'''
+# output_file('gapminder.html')
+# show(p)
+
 #################################################################
 # starting the app
 # Import the necessary modules
 
 # Make the ColumnDataSource: source
 src = ColumnDataSource(data={
-    'x'       : df[df['Year'] == 1970]['fertility'].values,
-    'y'       : df[df['Year'] == 1970]['life'].values,
-    'country' : df[df['Year'] == 1970]['Country'].values,
-    'pop'     : (df[df['Year'] == 1970]['population'].values / 20000000) + 2,
+    'x'       : df[df['Year'] == 1970]['fertility'],
+    'y'       : df[df['Year'] == 1970]['life'],
+    'country' : df[df['Year'] == 1970]['Country'],
+    'pop'     : (df[df['Year'] == 1970]['population'] / 20000000) + 2,
     'region'  : df[df['Year'] == 1970]['region'].values,
 })
 
@@ -49,20 +49,20 @@ ymin = min(df['fertility'])
 ymax = max(df['fertility'])
 
 # Create the figure: plot
-plot = figure(title='Gapminder Data for 1970', plot_height=400,                   plot_width=700, x_range=(xmin, xmax), 
-            y_range=(ymin, ymax))
+plot = figure(title='Gapminder Data for 1970', plot_height=400,                   plot_width=700, tools=[HoverTool(tooltips='@country')])
 
 # Add circle glyphs to the plot
 plot.circle(x='x', y='y', fill_alpha=0.8, source=src)
 
 # Set the x-axis label
-plot.xaxis.axis_label ='Fertility (children per woman)'
+# plot.xaxis.axis_label ='Fertility (children per woman)'
 
 # Set the y-axis label
-plot.yaxis.axis_label = 'Life Expectancy (years)'
+# plot.yaxis.axis_label = 'Life Expectancy (years)'
 
 # Add the plot to the current document and add a title
-# curdoc().add_root(plot)
-# curdoc().title = 'Gapminder'
+curdoc().add_root(plot)
+curdoc().title = 'Gapminder'
 
-show(plot)
+# Output the file and show the figure
+# show(plot)
